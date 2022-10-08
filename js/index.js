f2b2236303fa0a801866ae55533286e8e0936316
DiegoBorja09 <diegoborja09@gmail.com>
2022-10-07 23:08:28 -0500

    cambios

---
+++ js/index.js
@@ -0,0 +1,20 @@
+window.sr =ScrollReveal();
+
+sr.reveal('.about',{
+    duration:4000,
+    origin:'top',
+    distance:'-100px'
+
+})
+
+
+
+sr.reveal('.portafolio',{
+    duration:5000,
+    origin:'top',
+    distance:'-100px'
+
+})
+
+
+
