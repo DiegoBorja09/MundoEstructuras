bafd1abad5d4b6df1fa58da478ed3264d33e9c49
DiegoBorja09 <diegoborja09@gmail.com>
2022-10-10 21:33:30 -0500

    Cambios

--- js/index.js
+++ js/index.js
@@ -1,20 +1,41 @@
 window.sr =ScrollReveal();
 
-sr.reveal('.about',{
+sr.reveal('.about-me',{
     duration:4000,
     origin:'top',
-    distance:'-100px'
+    distance:'-80px'
 
 })
 
-
-
-sr.reveal('.portafolio',{
+sr.reveal('.services-reveal',{
     duration:5000,
     origin:'top',
-    distance:'-100px'
+    distance:'-80px'
 
 })
 
+sr.reveal('.portafolio-reveal',{
+    duration:5000,
+    origin:'top',
+    distance:'-80px'
+
+})
+
+
+sr.reveal('.about-rigth',{
+    duration:7000,
+    origin:'rigth',
+    distance:'-20px'
+
+})
+
+sr.reveal('.about-left',{
+    duration:7000,
+    origin:'left',
+    distance:'-20px'
+
+})
+
+     
 
 
