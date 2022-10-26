8aeeefef7482dbcb515a049cb9d75e410c07d841
DiegoBorja09 <diegoborja09@gmail.com>
2022-10-26 12:18:57 -0500

    cambios

--- js/index.js
+++ js/index.js
@@ -1,40 +1,74 @@
 window.sr =ScrollReveal();
-
-sr.reveal('.about-me',{
-    duration:4000,
-    origin:'top',
-    distance:'-80px'
-
-})
-
-sr.reveal('.services-reveal',{
+sr.reveal('.main--img',{
     duration:5000,
-    origin:'top',
-    distance:'-80px'
+    origin:'rigth',
+    distance:'-100px'
 
 })
-
-sr.reveal('.portafolio-reveal',{
+sr.reveal('.main--content',{
     duration:5000,
-    origin:'top',
-    distance:'-80px'
+    origin:'left',
+    distance:'-100px'
 
 })
-
 
 sr.reveal('.about-rigth',{
-    duration:7000,
+    duration:5000,
     origin:'rigth',
-    distance:'-20px'
+    distance:'-100px'
 
 })
 
 sr.reveal('.about-left',{
-    duration:7000,
+    duration:5000,
     origin:'left',
-    distance:'-20px'
+    distance:'-100px'
 
 })
+
+sr.reveal('#skill__title2',{
+    duration:5000,
+    origin:'top',
+    distance:'-100px'
+
+})
+sr.reveal('#card1',{
+    duration:5000,
+    origin:'rigth',
+    distance:'-100px'
+
+})
+
+sr.reveal('#card2',{
+    duration:5000,
+    origin:'top',
+    distance:'-100px'
+
+})
+sr.reveal('#card3',{
+    duration:5000,
+    origin:'left',
+    distance:'-100px'
+
+})
+
+
+sr.reveal('#skill__title',{
+    duration:5000,
+    origin:'top',
+    distance:'-100px'
+
+})
+sr.reveal('.contac',{
+    duration:5000,
+    origin:'top',
+    distance:'-100px'
+
+})
+
+
+
+
 
 
 window.addEventListener("load",function(){
