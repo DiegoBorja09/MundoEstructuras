52c2970d2613a069384d807d6c4b4778d93c4531
DiegoBorja09 <diegoborja09@gmail.com>
2022-11-04 15:00:31 -0500

    cambios

--- js/index.js
+++ js/index.js
@@ -5,55 +5,32 @@ sr.reveal('.main--img',{
     distance:'-10px'
 
 })
-sr.reveal('.main--content',{
-    duration:5000,
-    origin:'left',
-    distance:'-10px'
 
-})
 
 sr.reveal('.about-rigth',{
     duration:5000,
     origin:'rigth',
-    distance:'-10px'
+    distance:'-20px'
 
 })
 
 sr.reveal('.about-left',{
     duration:5000,
     origin:'left',
-    distance:'-10px'
+    distance:'-20px'
 
 })
 
-sr.reveal('#skill__title2',{
+
+
+
+sr.reveal('.services-reveal',{
     duration:5000,
     origin:'top',
     distance:'-50px'
 
 })
-sr.reveal('#card1',{
-    duration:5000,
-    origin:'rigth',
-    distance:'-10px'
-
-})
-
-sr.reveal('#card2',{
-    duration:5000,
-    origin:'top',
-    distance:'-50px'
-
-})
-sr.reveal('#card3',{
-    duration:5000,
-    origin:'left',
-    distance:'-10px'
-
-})
-
-
-sr.reveal('#skill__title',{
+sr.reveal('.portafolio-reveal',{
     duration:5000,
     origin:'top',
     distance:'-50px'
